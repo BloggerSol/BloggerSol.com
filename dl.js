abf287428560f57cb998bab8ae4d3ff3f7f2ba57
BloggerSol <116387941+BloggerSol@users.noreply.github.com>
2024-03-04 21:04:09 +0500

    Delete dl.js

--- dl.js
+++
@@ -1,2 +0,0 @@
-var timeLeft=20;function updateTimer(){var e=timeLeft<10?"0"+timeLeft:timeLeft;document.getElementById("timer").innerHTML=e,--timeLeft<0&&(clearInterval(x),document.getElementById("timer").classList.add("hidden"),document.getElementById("download").classList.remove("hidden"))}var x=setInterval(updateTimer,1e3);
-function refreshPage(){window.location.reload();} 
