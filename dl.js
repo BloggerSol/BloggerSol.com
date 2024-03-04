73234b269e3756cc4c16e1e0fdc17c525c23506d
BloggerSol <116387941+BloggerSol@users.noreply.github.com>
2024-03-04 21:01:30 +0500

    Update dl.js

--- dl.js
+++ dl.js
@@ -1,2 +1,2 @@
-var timeLeft=2;function updateTimer(){var e=timeLeft<10?"0"+timeLeft:timeLeft;document.getElementById("timer").innerHTML=e,--timeLeft<0&&(clearInterval(x),document.getElementById("timer").classList.add("hidden"),document.getElementById("download").classList.remove("hidden"))}var x=setInterval(updateTimer,1e3);
+var timeLeft=20;function updateTimer(){var e=timeLeft<10?"0"+timeLeft:timeLeft;document.getElementById("timer").innerHTML=e,--timeLeft<0&&(clearInterval(x),document.getElementById("timer").classList.add("hidden"),document.getElementById("download").classList.remove("hidden"))}var x=setInterval(updateTimer,1e3);
 function refreshPage(){window.location.reload();} 
